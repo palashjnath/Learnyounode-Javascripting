b73a15ee826ddf15f59c22a03cbe84b3afb6acad
Palash Nath <detroithumanpart2@gmail.com>
2020-06-06 21:39:13 +0530

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,5 @@
-console.log("HELLO WORLD")
+ //Create a file named hello-world.js. Write a program that prints the text "HELLO WORLD" to the console (stdout).  
+
+  
+  console.log("HELLO WORLD")
 
