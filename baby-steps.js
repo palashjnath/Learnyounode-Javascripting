6daf2bffa4c10e35b07805b57d389cf44fc1d979
Palash Nath <detroithumanpart2@gmail.com>
2020-06-06 21:40:46 +0530

    Update baby-steps.js

--- baby-steps.js
+++ baby-steps.js
@@ -1,3 +1,6 @@
+//Create a file named baby-steps.js.  
+//Write a program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout). 
+  
 var total = 0
 for (var i = 2; i < process.argv.length; i++){
 	total += +process.argv[i]
