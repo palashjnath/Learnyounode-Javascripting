5df1e1d3577b33d9fc00c749c52949b4417fb1a7
Palash Nath <detroithumanpart2@gmail.com>
2020-06-07 22:08:18 +0530

    Update http-client.js

--- http-client.js
+++ http-client.js
@@ -1,3 +1,8 @@
+/*Create a file named http-client.js.  
+   
+  Write a program that performs an HTTP GET request to a URL provided to you  
+  as the first command-line argument. Write the String contents of each  
+  "data" event from the response to a new line on the console (stdout).*/
 const http = require('http')
 const site = process.argv[2]
 http.get(site, function callback (response){
